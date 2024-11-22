ae7604458a897b1fad7e7155b214e5331618503f
TravailAuBlack <yassine82600@gmail.com>
2024-11-22 11:09:31 +0100

    Test 2

--- src/fizzbuzz/FizzBuzz.java
+++ src/fizzbuzz/FizzBuzz.java
@@ -3,7 +3,9 @@ package fizzbuzz;
 public class FizzBuzz {
 	
 	public static Object fizzbuzz(int i) {
-		return "1";
+		return "" + i;
 	}
+	
+	
 
 }
--- test/fizzbuzz/FizzBuzzTest.java
+++ test/fizzbuzz/FizzBuzzTest.java
@@ -9,8 +9,19 @@ import org.junit.jupiter.api.Test;
 public class FizzBuzzTest {
 	
 	@Test
-	void test() {
+	void fizz_un() {
 		assertEquals(FizzBuzz.fizzbuzz(1), "1");
+	}
+	
+	@Test
+	void fizz_deux() {
+		assertEquals(FizzBuzz.fizzbuzz(2),"2");
+	}
+	
+	
+	@Test
+	void fizz_trois() {
+		assertEquals(FizzBuzz.fizzbuzz(3), "fizz");
 	}
 
 }
