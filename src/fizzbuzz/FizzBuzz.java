a11b6631e2149d92094f2ffe1f979b2f01061e62
TravailAuBlack <yassine82600@gmail.com>
2024-11-22 11:22:33 +0100

    Test a ajouter

--- src/fizzbuzz/FizzBuzz.java
+++ src/fizzbuzz/FizzBuzz.java
@@ -1,13 +1,27 @@
 package fizzbuzz;
 
 public class FizzBuzz {
+	static int[] divisible = {3,5};
+	static String[] mots = {"fizz", "buzz"};
 	
-	public static Object fizzbuzz(int i) {
+	
+	public static String fizzbuzz(int entree) {
+		StringBuilder res = new StringBuilder();
 		
-		if(i==3)
-			return "fizz";
-		return  Integer.toString(i);
+		for(int i=0; i<divisible.length; i++) {
+			if(entree % divisible[i] == 0)
+				res.append(mots[i]);
+					
+		}
+		
+		if(res.length() == 0)
+			return Integer.toString(entree);
+		else 
+			return res.toString();
+		
 	}
+	
+	
 	
 	
 
